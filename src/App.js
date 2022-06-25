5fe1c88f40009b73f26de7ecd69cdb19ede07bee
Ishaan <ishaanshetty12@gmail.com>
2022-06-26 00:29:26 +0530

    Files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
-/* import './App.css'; */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
 
 function App() {
-  return <div className="App"></div>;
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        /* concept of promises used here */
+        setCoins(res.data);
+      })
+      .catch((error) => alert("yoo error"));
+  }, []); /* we put in an empy array so that we render it only once */
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  ); /* This is basically used to get the coin we search for */
+
+  return (
+    <div className="coin-app">
+      {" "}
+      {/* creating our JSX */}
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            price={coin.current_price}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
